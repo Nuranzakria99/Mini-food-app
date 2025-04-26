4bfd2b7cc9c411686c5e9d388b08da79aa1a4f6e
nuranzakria99 <nuranzakria99@gmail.com>
2025-04-26 15:50:26 +0300

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logoImage from "../assets/logoimage.png";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center p-4  text-white">
+      <div className="flex items-center gap-1">
+        <img src={logoImage} alt="Logo" className="" width={50} height={50} />
+        <h1 className="text-red-500 text-md font-bold">Foodio</h1>
+      </div>
+      <div>
+        <button className="text-black bg-red-500 p-2 rounded-lg text-md">Cart 0</button>
+      </div>
+    </header>
+  );
+}
---
+++ src/components/maels.jsx
@@ -0,0 +1,5 @@
+export default function Maels(){
+    return(
+        <div>hii</div>
+    )
+}
